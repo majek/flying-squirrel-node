63242411fa14c4470dff87a1ed4c430ece46b2f7
Marek Majkowski <majek04@gmail.com>
2011-07-15 16:13:37 +0100

    Move everything to ./lib.

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var http = require('http');
 var $ = require('jquery');
 
-require('./protocols').start();
-
-var webjs = require('./webjs');
-var endpoints = require('./endpoints');
-var tickets = require('./tickets');
 
 var config = require('./config');
-// Config must provide:
-//   config.server.port
-//   config.server.listenip (optional)
 
-var endpoint_path = [/[/]endpoints[/]([^/]+)[/]?$/, 'endpoint_name'];
-var ticket_path   = [/[/]endpoints[/]([^/]+)[/]tickets[/]?$/, 'endpoint_name'];
+
+var protocols = require('./lib/protocols');
+protocols.start(config);
+
+var webjs = require('./lib/webjs');
+var endpoints = require('./lib/endpoints');
+var tickets = require('./lib/tickets');
+
+
+var endpoint_path = [/^[/]endpoints[/]([^/]+)[/]?$/, 'endpoint_name'];
+var ticket_path   = [/^[/]endpoints[/]([^/]+)[/]tickets[/]?$/, 'endpoint_name'];
 
 var dispatcher = [
-    ['GET',  /[/]endpoints[/]?$/, ['list_endpoints', 'expose_endpoint']],
+    ['GET',  /^[/]endpoints[/]?$/, ['list_endpoints', 'expose_endpoint']],
     ['PUT',    endpoint_path, ['expect_json', 'put_endpoint', 'expose_endpoint']],
     ['GET',    endpoint_path, ['find_endpoint', 'get_endpoint', 'expose_endpoint']],
     ['DELETE', endpoint_path, ['find_endpoint', 'delete_endpoint', 'expose_json']],
